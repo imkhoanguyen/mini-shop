580b13ca8d357596c11e73db8c39a0351a0bc029
tunguyen <nguyenthanhthientu18nt@gmail.com>
2024-09-16 21:47:11 +0700

    merge tudeptrai

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -17,7 +17,7 @@ namespace API.Extensions
         {
             services.AddDbContext<StoreContext>(opt =>
             {
-                opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
             var cloudinarySettings = config.GetSection("CloudinarySettings").Get<CloudinarySettings>();
             var cloudinaryAccount = new Account(
