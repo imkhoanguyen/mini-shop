6c045a25b12621514e44345a7c09e80e0dce25c6
tunguyen <nguyenthanhthientu18nt@gmail.com>
2024-10-15 17:31:53 +0700

    commit

---
+++ Client/src/app/component/admin/product/add-variant/add-variant.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-variant',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-variant.component.html',
+  styleUrl: './add-variant.component.css'
+})
+export class AddVariantComponent {
+
+}
