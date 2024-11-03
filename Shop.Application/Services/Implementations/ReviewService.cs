0b1c89ee17d47c3f0085c6bba04f9a43e4a3a2c9
khoa <khoanguyen.coder@gmail.com>
2024-11-03 09:42:00 +0700

    feat: add review service

---
+++ Shop.Application/Services/Implementations/ReviewService.cs
@@ -0,0 +1,13 @@
+﻿using Shop.Application.Services.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Services.Implementations
+{
+    public class ReviewService : IReviewService
+    {
+    }
+}
