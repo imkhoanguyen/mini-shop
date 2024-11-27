a42928515504f4b045faf933b4086f6303b2f6a2
tunguyen <nguyenthanhthientu18nt@gmail.com>
2024-11-27 15:41:45 +0700

    product params

--- Shop.Application/Parameters/ProductParams.cs
+++ Shop.Application/Parameters/ProductParams.cs
@@ -4,7 +4,8 @@ namespace API.Helpers
 {
     public class ProductParams : BaseParams
     {
-        public int SelectedSize { get; set; }
+        public List<int>? SelectedSize { get; set; }
         public int SelectedColor { get; set; }
+        public List<int>? SelectedCategory { get; set; }
     }
 }
