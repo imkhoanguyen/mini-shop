0337f052727dd032db721b5eb553bfd939f0e32f
khoa <khoanguyen.coder@gmail.com>
2024-11-29 11:58:19 +0700

    feat: complete ui checkout

--- Client/src/app/component/client/checkout/checkout.component.ts
+++ Client/src/app/component/client/checkout/checkout.component.ts
@@ -6,6 +6,10 @@ import { AccountService } from '../../../_services/account.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ShippingMethodService } from '../../../_services/shippingMethod.service';
+import { ShippingMethodDto } from '../../../_models/shippingMethod.module';
+import { DiscountService } from '../../../_services/discount.service';
+import { DiscountDto } from '../../../_models/discount.module';
 
 @Component({
   selector: 'app-checkout',
@@ -19,17 +23,32 @@ export class CheckoutComponent implements OnInit {
   private addressService = inject(AddressService);
   accountService = inject(AccountService);
   private router = inject(Router);
+  private shippingMethodService = inject(ShippingMethodService);
+  private discountService = inject(DiscountService);
+
   addressList: Address[] = [];
+  shippingMethodList: ShippingMethodDto[] = [];
+  discountCode = '';
   order: any = {
-    address: '',
     description: '',
+    shippingFee: 0,
+    address: '',
+    fullName: '',
+    phoneNumber: '',
+    discountId: 0,
+    discountPrice: 0,
   };
+
+  selectedAddress = 0;
+  currentDiscount!: DiscountDto | null;
 
   ngOnInit(): void {
     if (!this.accountService.currentUser()) {
       this.router.navigateByUrl('/login');
     }
     this.loadAddress();
+    this.loadShippingMethod();
+    console.log(this.shippingMethodList[0].cost);
   }
 
   loadAddress() {
@@ -45,7 +64,44 @@ export class CheckoutComponent implements OnInit {
       });
   }
 
+  loadShippingMethod() {
+    this.shippingMethodService.getAllShippingMethods().subscribe({
+      next: (res) => {
+        this.shippingMethodList = res;
+        this.order.shippingFee = res[0].cost;
+      },
+      error: (er) => {
+        console.log(er);
+      },
+    });
+  }
+
   onChangeAddress() {
-    console.log(this.order.address);
+    const address = this.addressList.find((a) => a.id == this.selectedAddress);
+    this.order.address =
+      address?.street + ' - ' + address?.district + ' - ' + address?.city;
+
+    this.order.fullName = address?.fullName;
+    this.order.phoneNumber = address?.phone;
+  }
+
+  onShippingMethodChange(item: ShippingMethodDto) {
+    console.log('selected shopping method', item);
+    this.order.shippingFee = item.cost;
+  }
+
+  onPaymentMethodChange(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    console.log('Selected payment method:', value);
+  }
+
+  applyDiscount() {
+    this.discountService.getDiscountByCode(this.discountCode).subscribe({
+      next: (res) => {
+        this.currentDiscount = res.body;
+      },
+      error: (er) => console.log(er),
+    });
+    console.log(this.discountCode);
   }
 }
