ff577b588f18eaf8b1b266a750c13fb1ee3bcf1d
khoa <khoanguyen.coder@gmail.com>
2024-12-03 23:54:58 +0700

    refactor: change auth guard with role to claim

--- Client/src/app/_guards/auth.guard.ts
+++ Client/src/app/_guards/auth.guard.ts
@@ -1,28 +1,37 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from '../_services/toastr.service';
 import { map, catchError, of, from } from 'rxjs';
+import { AuthService } from '../_services/auth.service';
 
-export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+export const authGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
   const accountService = inject(AccountService);
+  const authService = inject(AuthService);
   const router = inject(Router);
   const toastrService = inject(ToastrService);
 
-  return from(accountService.isCustomerRole()).pipe(
-    map((isCustomer) => {
-      const currentUser = accountService.getCurrentUser();
-      if (currentUser && !isCustomer) {
-        return true;
-      }
-      toastrService.error('Quyền truy cập bị từ chối');
-      router.navigate(['/']);
-      return false;
-    }),
-    catchError(() => {
-      toastrService.error('Có lỗi xảy ra, vui lòng thử lại');
-      router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-      return of(false);
-    })
-  );
+  const currentUser = accountService.getCurrentUser();
+  const isAccessAdminPage = authService.hasClaim('Access.Admin');
+
+  if (currentUser && isAccessAdminPage) {
+    return true;
+  }
+
+  if (!currentUser) {
+    toastrService.error('Bạn cần đăng nhập');
+    router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+  } else {
+    toastrService.error('Quyền truy cập bị từ chối');
+    router.navigate(['/']);
+  }
+  return false;
 };
