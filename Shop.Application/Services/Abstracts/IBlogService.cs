974c8a92166aa0bcedda6cdedfe6d250012a68bc
viet <ngcanhviet@gmail.com>
2024-12-04 09:47:49 +0700

    update blog api

--- Shop.Application/Services/Abstracts/IBlogService.cs
+++ Shop.Application/Services/Abstracts/IBlogService.cs
@@ -11,11 +11,13 @@ namespace Shop.Application.Services.Abstracts
     public interface IBlogService
     {
         Task<IEnumerable<Blog>> GetAllBlogsAsync(bool tracked);
+        Task<IEnumerable<Blog>> GetAll5BlogsAsync(bool tracked);
         Task<PagedList<Blog>> GetAllBlogsAsync(BlogParams blogParams, bool tracked = false);
         Task<Blog?> GetBlogsById(int id);
         Task DeleteAsync(Blog blog);
         Task UpdateBlogAsync(Blog blog);
 
         Task AddAsync(Blog blog);
+
     }
 }
